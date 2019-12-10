79da1c5eef76b1163ca037571d609e25bc0e6822
Derek Kedziora <derekkedziora@icloud.com>
2019-12-11 00:40:43 +0200

    bug in dark mode toggle fixed

--- assets/scripts/mode-switcher.js
+++ assets/scripts/mode-switcher.js
@@ -1,18 +1,20 @@
-const userPrefers = getComputedStyle(document.documentElement).getPropertyValue('content');	
+const systemDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+let currentMode = document.documentElement.getAttribute('data-theme');
+	
+	if (currentMode === null && systemDarkTheme) {
+		document.getElementById("theme-toggle").innerHTML = "Light Mode";
+		document.documentElement.setAttribute('data-theme', 'dark');
+		window.localStorage.setItem('theme', 'dark');
+	} else if (currentMode === null && systemDarkTheme === false) {
+		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+		document.documentElement.setAttribute('data-theme', 'light');
+		window.localStorage.setItem('theme', 'light');
+	} else if (currentMode === "dark") {
+		document.getElementById("theme-toggle").innerHTML = "Light Mode";
+	} else if (currentMode === "light") {
+		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+	}
 
-if (theme === "dark") {
-	document.getElementById("theme-toggle").innerHTML = "Light Mode";
-} else if (theme === "light") {
-	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
-} else if  (userPrefers === "dark") {
-	document.documentElement.setAttribute('data-theme', 'dark');
-	window.localStorage.setItem('theme', 'dark');
-	document.getElementById("theme-toggle").innerHTML = "Light Mode";
-} else {
-	document.documentElement.setAttribute('data-theme', 'light');
-	window.localStorage.setItem('theme', 'light');
-	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
-}
 
 function modeSwitcher() {
 	let currentMode = document.documentElement.getAttribute('data-theme');

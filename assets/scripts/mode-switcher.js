cec3a25c0efb27616127f2394887f3dc0a8518df
derekkedziora <derekkedziora@icloud.com>
2019-12-11 01:16:28 +0200

    Local storage changed to session storage

--- assets/scripts/mode-switcher.js
+++ assets/scripts/mode-switcher.js
@@ -3,12 +3,8 @@ let currentMode = document.documentElement.getAttribute('data-theme');
 	
 	if (currentMode === null && systemDarkTheme) {
 		document.getElementById("theme-toggle").innerHTML = "Light Mode";
-		document.documentElement.setAttribute('data-theme', 'dark');
-		window.localStorage.setItem('theme', 'dark');
 	} else if (currentMode === null && systemDarkTheme === false) {
 		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
-		document.documentElement.setAttribute('data-theme', 'light');
-		window.localStorage.setItem('theme', 'light');
 	} else if (currentMode === "dark") {
 		document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	} else if (currentMode === "light") {
@@ -20,11 +16,19 @@ function modeSwitcher() {
 	let currentMode = document.documentElement.getAttribute('data-theme');
 	if (currentMode === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
-		window.localStorage.setItem('theme', 'light');
+		sessionStorage.setItem('theme', 'light');
+		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+	} else if (currentMode === "light") {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		sessionStorage.setItem('theme', 'dark');
+		document.getElementById("theme-toggle").innerHTML = "Light Mode";
+	} else if (currentMode === null && systemDarkTheme) {
+		document.documentElement.setAttribute('data-theme', 'light');
+		sessionStorage.setItem('theme', 'light');
 		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
-		window.localStorage.setItem('theme', 'dark');
+		sessionStorage.setItem('theme', 'dark');
 		document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	}
 }

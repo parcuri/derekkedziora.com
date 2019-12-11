042a2f02bdc05403733f164f881816abcc3a57e3
derekkedziora <derekkedziora@icloud.com>
2019-12-11 08:51:31 +0200

    improvements to mode swither

--- assets/scripts/mode-switcher.js
+++ assets/scripts/mode-switcher.js
@@ -1,13 +1,23 @@
-const systemDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
-let currentMode = document.documentElement.getAttribute('data-theme');
+const theme = sessionStorage.getItem('theme');
+	if (theme === "dark") {
+    	document.documentElement.setAttribute('data-theme', 'dark');
+    } else if (theme === "light") {
+    	document.documentElement.setAttribute('data-theme', 'light');  
+    }
+
+
+	const systemDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+	let currentMode = document.documentElement.getAttribute('data-theme');
 	
 	if (currentMode === null && systemDarkTheme) {
 		document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	} else if (currentMode === null && systemDarkTheme === false) {
 		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 	} else if (currentMode === "dark") {
+		document.documentElement.setAttribute('data-theme', 'dark');
 		document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	} else if (currentMode === "light") {
+		sessionStorage.setItem('theme', 'light');
 		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 	}
 

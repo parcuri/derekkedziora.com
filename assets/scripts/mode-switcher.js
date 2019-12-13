2da86c2ef0abe1e99838e21fb37f25fbe145b1dc
derekkedziora <derekkedziora@icloud.com>
2019-12-13 20:18:23 +0200

    minor tweaks

--- assets/scripts/mode-switcher.js
+++ assets/scripts/mode-switcher.js
@@ -22,6 +22,7 @@ systemInitiatedDark.addListener(prefersColorTest);
 
 
 function modeSwitcher() {
+	let theme = sessionStorage.getItem('theme');
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
@@ -30,9 +31,11 @@ function modeSwitcher() {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
 		document.getElementById("theme-toggle").innerHTML = "Light Mode";
-	} else if (systemInitiatedDark.matches) {
+	} else if (systemInitiatedDark.matches) {	
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
+		//let theme = sessionStorage.getItem('theme');
+		//console.log("this was triggered");
 		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
